fa872dda963ff2e416d8ffce854e9e71ab830bf8
EthanHess <ech1988@gmail.com>
2016-03-16 12:35:04 -0700

    first commit

---
+++ BranchTest/BranchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BranchTest
+//
+//  Created by Ethan Hess on 3/16/16.
+//  Copyright © 2016 Ethan Hess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
